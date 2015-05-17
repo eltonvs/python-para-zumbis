9580b5ff04fc70ef8bdc0f22346d2d8fbfaa1e3e
Elton Viana <eltonvs@outlook.com>
2015-05-17 15:02:45 -0300

    Create questao03.py

---
+++ Lista2/questao03.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+'''
+João Papo-de-Pescador, homem de bem, comprou um microcomputador para controlar o rendimento diário de seu trabalho.
+Toda vez que ele traz um peso de peixes maior que o estabelecido pelo regulamento de pesca do estado de São Paulo (50 quilos) deve pagar uma multa de R$ 4,00 por quilo excedente.
+João precisa que você faça um programa que leia a variável peso (peso de peixes) e verifique se há excesso.
+Se houver, gravar na variável excesso e na variável multa o valor da multa que João deverá pagar.
+Caso contrário mostrar tais variáveis com o conteúdo ZERO.
+'''
+
+peso = float(input("Peso dos Peixes: "))
+
+if peso > 50:
+	excesso = peso - 50
+	multa = excesso * 4
+	print ("O excesso foi de %.2f Kg e a multa é de R$ %.2f" %(excesso, multa))
+else:
+	excesso = "ZERO"
+	multa = "ZERO"
+	print ("O excesso foi de", excesso, "Kg e a multa é de", multa, "reais")

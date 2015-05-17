cc27b2026208e2dceb2456e9339321d850a10ef9
Elton Viana <eltonvs@outlook.com>
2015-05-17 14:43:46 -0300

    Create questao08.py

---
+++ Lista1/questao08.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+'''
+Faça agora o contrário, de Fahrenheit para Celsius.
+'''
+
+fahrenheit = float(input("Digite a temperatura em Fahrenheit: "))
+celsius = (fahrenheit - 32) * 1.8
+
+print ("A temperatura em Celsius é %.2f °C" %celsius)

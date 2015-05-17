7c82c31f95169d2354f1a10f97d3d21f89dc5ac5
Elton Viana <eltonvs@outlook.com>
2015-05-17 14:28:48 -0300

    Create questao01.py

---
+++ Lista1/questao01.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+'''
+Faça um programa que peça dois números inteiros e imprima a soma desses dois números
+'''
+
+n1 = int(input("Número 1: "))
+n2 = int(input("Número 2: "))
+print("%d + %d = %d" %(n1, n2, n1 + n2))

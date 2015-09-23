cd79ca34ba718da5d57ebfbfb44c6cea14730da3
Elton Viana <eltonvs@outlook.com>
2015-09-23 18:54:36 -0300

    Create questao04.py

---
+++ Lista4/questao04.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+'''
+Seja o statement sobre diversidade:
+“The Python Software Foundation and the global Python community welcome and encourage participation by everyone.
+Our community is based on mutual respect, tolerance, and encouragement,
+and we are working to help each other live up to these principles.
+We want our community to be more diverse: whoever you are,
+and whatever your background, we welcome you.”.
+Gere uma lista de palavras deste texto com split(),
+a seguir crie uma lista com as palavras que começam ou terminam com uma das letras “python”.
+Imprima a lista resultante.
+Não se esqueça de remover antes os caracteres especiais e cuidado com maiúsculas e minúsculas.
+'''
+
+from re import sub
+
+text = '''
+The Python Software Foundation and the global Python community welcome and encourage participation by everyone.
+Our community is based on mutual respect, tolerance, and encouragement,
+and we are working to help each other live up to these principles.
+We want our community to be more diverse: whoever you are,
+and whatever your background, we welcome you.
+'''
+text = sub('[!@#$,.]', '', text)
+words = text.split()
+letters = list('python')
+l = []
+
+for i in words:
+	if i[0].lower() in letters or i[-1] in letters: l.append(i)
+
+print(l)

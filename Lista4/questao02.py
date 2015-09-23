aa33baae4801fbf3e2c458772da918ca4ff7ea47
Elton Viana <eltonvs@outlook.com>
2015-09-23 18:31:59 -0300

    Visual and performance improvements
    Import only the randint from random library.
    Removed line break from if/else.
    Removed whitespaces between the print and brackets.

--- Lista4/questao02.py
+++ Lista4/questao02.py
@@ -7,17 +7,15 @@ Armazene os números pares na lista PAR e os números ímpares na lista IMPAR.
 Imprima as três listas.
 '''
 
-import random
+from random import randint
 
 num, par, impar = [], [], []
 
-for i in range(0, 20):
-	num.append(random.randint(1, 100))
-	if num[i] % 2 == 0:
-		par.append(num[i])
-	else:
-		impar.append(num[i])
+for i in range(20):
+	num.append(randint(1, 100))
+	if num[i] % 2 == 0: par.append(num[i])
+	else: impar.append(num[i])
 
-print ("Os números sorteados foram:", num)
-print ("São pares:", par)
-print ("São ímpares:", impar)
+print("Os números sorteados foram:", num)
+print("São pares:", par)
+print("São ímpares:", impar)
